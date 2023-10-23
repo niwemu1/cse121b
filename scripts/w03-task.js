0b7e4ef8d6acb3a7316427bf6537c6b427e2031c
niwemu1 <niwemugenideborah@gmail.com>
2023-10-23 10:21:26 -0700

    Make changes

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -4,56 +4,52 @@
 /* Function Definition - Add Numbers */
 
 function add(number1,number2){
-    return number1+number2;
+     return number1 + number2;
     }
-    
-    function addnumbers(){
-     addend1=parseInt(document.getElementById('addend1').value);
-     addend2=parseInt(document.getElementById('addend2').value);
-     sum = add(addend1,addend2);
-     
-     document.getElementById('sum').value = sum;
-    }
-
-    const element = document.getElementById('addNumbers');
-    element.addEventListener('click',addnumbers);
+function addNumbers(){
+     addend1 = document.getElementById('add1').value;
+     addend2 = document.getElementById('add2').value;
+     addend = add(addend1,addend2)
+     return document.getElementById('sum').value = addend;
+    }   
+    document.getElementById('addNumbers').addEventListener('click', addNumbers);   
 
 /* Function Expression - Subtract Numbers */
 function substract(number1,number2){
     return number1-number2;
 }
-function subtractnumbers(){
-    minuend=document.getElementById('minuend').value;
-    subtrahend=document.getElementById('subtrahend').value;
+function subtractNumbers(){
+    minuend=document.getElementById('subtract1').value;
+    subtrahend=document.getElementById('subtract2').value;
     diff= substract(minuend,subtrahend);
     return document.getElementById('difference').value = diff;
    }   
-   document.getElementById('subtractNumbers').addEventListener('click',subtractnumbers);   
+   document.getElementById('subtractNumbers').addEventListener('click',subtractNumbers);   
 
 /* Arrow Function - Multiply Numbers */
 function multiply(number1,number2){
     return number1*number2;
 }
-function multiplynumbers(){
+function multiplyNumbers(){
     factor1=document.getElementById('factor1').value;
     factor2=document.getElementById('factor2').value;
     mult = multiply(factor1,factor2);
     return document.getElementById('product').value= mult;
 }
-document.getElementById('multiplyNumbers').addEventListener('click',multiplynumbers);
+document.getElementById('multiplyNumbers').addEventListener('click',multiplyNumbers);
 
 /* Open Function Use - Divide Numbers */
 function divide(number1,number2){
     return number1/number2;
 }
-function dividenumbers(dividend,divisor){
+function divideNumbers(dividend,divisor){
     dividend=document.getElementById('dividend').value;
     divisor=document.getElementById('divisor').value;
     quot= divide(dividend,divisor);
     return document.getElementById('quotient').value= quot;
 
    } 
-   document.getElementById('divideNumbers').addEventListener('click',dividenumbers);
+   document.getElementById('divideNumbers').addEventListener('click',divideNumbers);
 
 /* Decision Structure */
 const today = new Date();
@@ -63,43 +59,42 @@ console.log(year);
 document.getElementById('year').innerHTML = year;
 
 /* ARRAY METHODS - Functional Programming */
-const allNumbers=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
-
+let numbersArray = [1,2,3,4,5,6,7,8,9,10,11,12,13];
 /* Output Source Array */
-document.getElementById('array').innerHTML= allNumbers;
+document.getElementById('array').innerHTML= numbersArray;
 
 /* Output Odds Only Array */
-const oddNumbers = allNumbers.filter((value)=>{
-    return value % 2 !== 0;
+const oddNumbers = numbersArray.filter((number)=>{
+    return number % 2 !== 0;
 })
 console.log(oddNumbers);
 document.getElementById('odds').innerHTML = oddNumbers
 
 /* Output Evens Only Array */
-const even = allNumbers.filter((value)=>{
-    return value % 2 === 0;
+const even = numbersArray.filter((number)=>{
+    return number % 2 === 0;
 })
 console.log(even);
 
 document.getElementById('evens').innerHTML = even
     
 /* Output Sum of Org. Array */
-const result = allNumbers.reduce((sum,value)=>{
-    return sum + value;
+const result = numbersArray.reduce((sum,number)=>{
+    return sum + number;
 })
 console.log(result);
 document.getElementById('sumOfArray').innerHTML = result
 
 /* Output Multiplied by 2 Array */
-const map = allNumbers.map(x => x * 2);
+const map = numbersArray.map(x => x * 2);
     
     console.log(map);
-    document.getElementById('multiplied').innerHTML=map ;
+    document.getElementById('multiplied').innerHTML = map ;
 
 /* Output Sum of Multiplied by 2 Array */
-const sum1 = map.reduce((sum,value)=>{
-    return sum + value;
+const sum1 = map.reduce((sum,number)=>{
+    return sum + number;
 })
 
 console.log(sum1);
-document.getElementById('sumOfMultiplied').innerHTML=sum1;
+document.getElementById('sumOfMultiplied').innerHTML = sum1;
